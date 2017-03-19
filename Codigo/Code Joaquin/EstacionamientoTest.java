84009b8f5931a660a35d19e70fdfb0efbb0d1b12
Joaquín Hernández <joaquin_downhill@hotmail.com>
2017-03-19 17:15:29 -0600

    cambios

--- Codigo/Code Joaquin/EstacionamientoTest.java
+++
@@ -1,8 +0,0 @@
-public class EstacionamientoTest {
-	public static void main(String[] args) {
-		Pensionados obj = new Pensionados();
-
-		obj.Datos();
-
-	}
-}
--- Codigo/Joaquin/EstacionamientoTest.java
+++ Codigo/Joaquin/EstacionamientoTest.java
@@ -2,7 +2,14 @@ public class EstacionamientoTest {
 	public static void main(String[] args) {
 		Pensionados obj = new Pensionados();
 
-		obj.Datos();
-
+		obj.contraFecha();
+		obj.vencifecha();
+		obj.nombre();
+		obj.tipoPension();
+		obj.cajon();
+		obj.placas();
+		System.out.println();
+		obj.printpension();
+		
 	}
 }
