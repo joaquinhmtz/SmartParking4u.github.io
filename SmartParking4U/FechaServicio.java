5ba67ae1b692d5f5b163f085bae1b856c5060b36
Joaquín Hernández <joaquin_downhill@hotmail.com>
2017-03-22 17:55:51 -0600

    Changes

--- SmartParking4U/FechaServicio.java
+++ SmartParking4U/FechaServicio.java
@@ -1,4 +1,31 @@
+import java.io.*;
+
 public class FechaServicio {
 
+	private String fechaContra;
+	private String fechaVenci;
 
+	private InputStreamReader isr = new InputStreamReader(System.in);
+	private BufferedReader br = new BufferedReader(isr);
+
+	public String pimpLeefechaContra() {
+		System.out.print("Fecha de contratación del servicio: ");
+		return leeFechacontra();
+	}
+	private String leeFechacontra() {
+		try{
+			fechaContra = br.readLine();
+		}catch(IOException e) {}
+		return fechaContra;
+	}
+	public String pimpLeefechaVenci() {
+		System.out.print("Fecha de vencimiento del servicio: ");
+		return leeFechavenci();
+	}
+	private String leeFechavenci() {
+		try{
+			fechaVenci = br.readLine();
+		}catch(IOException e) {}
+		return fechaVenci;
+	}
 }
--- SmartParking4U/Persona.java
+++ SmartParking4U/Persona.java
@@ -1,3 +1,21 @@
+import java.io.*;
+
 public class Persona {
+
+	private String nombre;
+
+	private InputStreamReader isr = new InputStreamReader(System.in);
+	private BufferedReader br = new BufferedReader(isr);
+
+	public String pimpLeeNombre() {
+		System.out.print("Nombre del propietario ls: ");
+		return leeNombre();
+	}
+	private String leeNombre() {
+		try{
+			nombre = br.readLine();
+		}catch(IOException e) {}
+		return nombre;
+	}
 
 }
